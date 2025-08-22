68a8921e0bc48bfc83c09c921ca717615a53fbe5
C0d3rv <vasuthec0der@gmail.com>
2025-08-22 16:20:32 +0530

    Added generate password feature. Added copy-to-clipboard feature

--- main.py
+++ main.py
@@ -1,13 +1,45 @@
 from tkinter import *
+from tkinter import messagebox
+import random
+import pyperclip
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    password_letters = [random.choice(letters) for _ in range(nr_letters)]
+    password_symbols = [random.choice(symbols) for _ in range(nr_symbols)]
+    password_numbers = [random.choice(numbers) for _ in range(nr_numbers)]
+    password_list = password_letters + password_symbols + password_numbers
+
+    random.shuffle(password_list)
+
+    password = "".join(password_list)
+    password_entry.insert(0, password)
+    pyperclip.copy(password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save(*args):
-    with open("saved_passwords.txt", "a") as file:
-        file.write(f"{web_entry.get()} | {email_entry.get()} | {password_entry.get()}\n")
-        web_entry.delete(0, END)
-        email_entry.delete(0, END)
-        password_entry.delete(0, END)
+    site = web_entry.get()
+    mail = email_entry.get()
+    passw = password_entry.get()
+
+    if (len(site) == 0)  or (len(mail)==0) or (len(passw)==0):
+        messagebox.showerror(title="Error!", message="Please enter a valid data")
+    else:
+        confirmation = messagebox.askyesno(title="Confirm", message=f"Do you want to save {passw} for {site}")
+        if confirmation:
+            with open("saved_passwords.txt", "a") as file:
+                file.write(f"{site} | {mail} | {passw}\n")
+                web_entry.delete(0, END)
+                password_entry.delete(0, END)
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
@@ -34,7 +66,7 @@ password = Label(text="Password:", bg="white", highlightthickness=0)
 password_entry = Entry(width=35, show="*")
 password.grid(row=3, column=0)
 password_entry.grid(row=3, column=1, columnspan=2)
-gen_pass = Button(text="Generate Password", width=15, bg="white", fg="black")
+gen_pass = Button(text="Generate Password", width=15, bg="white", fg="black", command=generate_password)
 gen_pass.grid(row=3, column=2)
 
 add_pass = Button(text="Add", width=30, bg="white", fg="black", command=save)
